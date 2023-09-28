65dee0e0859db282ad59e7bef200791bd0ea5dcf
Chandu-QA <TestChandu@gmail.com>
2023-09-28 21:42:41 +0530

    Methods

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Chandu
+    {
+        public void publicmethod1()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        private void privatemethod2()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        internal void internalmethod3()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected void protectedmethod4()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected internal void protectedinternalmethod5()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+
+        //
+        public static void publicmethod11()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        private static void privatemethod22()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        internal static void internalmethod33()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected static void protectedmethod44()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected static internal void protectedinternalmethod55()
+        {
+
+        }
+
+    }
+    public class Class1
+    {
+        //public int hd()
+        //{
+        //    return 10;
+        //}
+        //public string add()
+        //{
+        //    string variablename = "chandu2";
+        //    return variablename;
+        //}
+        //public void chandu1()
+        //{
+
+        //}
+        public void publicmethod()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        private void privatemethod()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        internal void internalmethod()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        public static void abcd()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected void protectedmethod()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        protected internal void protectedinternalmethod()
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+        }
+        public static void main(String[] args)
+        {
+            Class1 c1 = new Class1();
+            c1.publicmethod();
+            c1.internalmethod();
+            c1.protectedmethod();
+            c1.privatemethod();
+
+            //Chandu ch2 = new Chandu();
+            Chandu.internalmethod33();
+            Chandu.protectedinternalmethod55();
+            Chandu.publicmethod11();
+            Chandu.privatemethod();
+            Chandu.protectedinternalmethod();
+                Class1.abcd();
+                abcd();
+        }
+        public void Main(String[] args)
+        {
+            Chandu ch1 = new Chandu();
+            ch1.protectedinternalmethod5();
+            ch1.internalmethod3();
+            ch1.publicmethod1();
+            ch1.internalmethod3();
+            ch1.protectedinternalmethod5();
+            ch1.
+            
+            publicmethod();
+            abcd();
+        }
+
+    }
+}
